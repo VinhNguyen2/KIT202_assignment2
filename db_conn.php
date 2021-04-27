a49337947dee067bc9ac52135c2a3aec825d971d
Danh Tran <danhtran@Danhs-MBP.lan>
2021-04-28 00:24:27 +1000

    new files

--- db_conn.php
+++ db_conn.php
@@ -1,10 +1,31 @@
 <?php
 //connect to mysql
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = 'root';
+$db_db = 'information_schema';
+$db_port = 8889;
 
-$conn = new mysqli('localhost', 'root', 'root', 'group30_db');
-if (mysqli_connect_error()) {
-	    printf("Connect failed: %s\n", mysqli_connect_error());
-        echo "<script type='text/javascript'>alert('".addslashes($error)."');</script>";
-	    exit();
-	}
+
+$mysqli = new mysqli(
+    $db_host,
+    $db_user,
+    $db_password,
+    $db_db
+  );
+	
+  if ($mysqli->connect_error) {
+    echo 'Errno: '.$mysqli->connect_errno;
+    echo '<br>';
+    echo 'Error: '.$mysqli->connect_error;
+    exit();
+  }
+
+  echo 'Success: A proper connection to MySQL was made.';
+  echo '<br>';
+  echo 'Host information: '.$mysqli->host_info;
+  echo '<br>';
+  echo 'Protocol version: '.$mysqli->protocol_version;
+
+  $mysqli->close();
 ?>
