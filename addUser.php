d6cdd66d11f2948fd005cb69177752a7be64d262
VinhNguyen2 <46787486+VinhNguyen2@users.noreply.github.com>
2021-04-29 09:27:32 +1000

    Completed add user to database on Manager page

---
+++ addUser.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+    <h2> Add new user </h2>
+</body>
+</html>
+
+<?php
+include("Registration.php");
+header( 'Location: Manager_page.php' );
+?>
+    <button><a href="Manager_page.php">Go back to main list</a></button>
+    
